46a60e898d09589c60e7cc14670035f124244d63
Thelin90 <simon.thelin90@gmail.com>
2018-11-04 19:59:41 +0000

    TD-0002/enable-tests:
    - Tests work fine in PyCharm, does not work through console, something wrong with PYTHONPATH

---
+++ tests/unit/app/modules/unittest-etl-process.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from .fakeSparkSession import PySparkTest
+from etl import extract
+
+class SimpleTest(PySparkTest):
+    """
+    Will perform test on the functions used within the ETL process with a fake spark session
+    """
+    def test_basic(self):
+        expected_df = extract(self.spark)
+
+        self.assertEquals(expected_df.schema.names, ["date", "time", "user_id", "url", "ip", "user_agent_string"])
